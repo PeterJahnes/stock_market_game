7abf9138e38e23890c0142964dac6d4a07aab87d
Peter Jahnes <peter.jahnes@gmail.com>
2015-07-06 15:36:44 -0400

    adding offset route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'stock_market_game#index'
+  get 'stock_market_game/offset' => 'stock_market_game#offset'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
