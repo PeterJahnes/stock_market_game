3c3e9475f9b2904ed7f13a898e72939c03faf649
Peter Jahnes <peter.jahnes@gmail.com>
2015-07-06 21:00:29 -0400

    creating controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,30 @@
 class SessionsController < ApplicationController
 
-	def login
+	def new
 	end
 
-	def loginp
+	def create
+		@user = User.find_by email: user_params[:email]
+		respond_to do |format|
+	    if @user && @user.authenticate(user_params[:password])
+	      format.html { redirect_to root_path, notice: 'Welcome.' }
+	      format.json { render json: {status: :created, name: @user.first_name} }
+	    else
+	      format.html { render :new }
+	      format.json { render json: {status: "failed to login"} }
+	    end
+    end
 	end
 
-	def logout
+	def destroy
 		session[:user_id] = nil
 		redirect_to root_path
 	end
+
+	private
+
+    def user_params
+      params.require(:session).permit(:email, :password)
+    end
 
 end
