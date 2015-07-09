f638f07f21e1c101bc2b8185dc166da8d72326b3
Peter Jahnes <peter.jahnes@gmail.com>
2015-07-09 15:58:10 -0400

    adding save and load game links

--- app/controllers/stock_market_game_controller.rb
+++ app/controllers/stock_market_game_controller.rb
@@ -1,6 +1,11 @@
 class StockMarketGameController < ApplicationController
 
+	include ApplicationHelper
+
 	def index
+		if !current_user
+	    @user = User.new
+	  end
 	end
 
 	def offset
@@ -8,5 +13,39 @@ class StockMarketGameController < ApplicationController
 			format.json { render json: {offset: Time.now.utc_offset}.to_json }
 		end
 	end
+
+
+	def save
+		if current_user
+			p "#"*20
+			p "*"*20
+			saved_game = game_params[:saved_game]
+			p "*"*20
+			@user = current_user
+			respond_to do |format|
+				if @user.update(saved_game: saved_game)
+					format.json { render json: {status: "success"}.to_json }
+				else
+					format.json { render json: {status: "failed to load game"}} 
+				end
+			end
+		end
+	end
+
+	def load
+		respond_to do |format|
+			if current_user
+				format.json { render json: {game: current_user.saved_game , stat: true}.to_json }
+			else
+				format.json { render json: {status: "failed to load game", stat: false} }
+			end
+		end
+	end
+
+	private
+
+    def game_params
+      params.require(:game).permit!
+    end
 
 end
